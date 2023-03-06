9b4ff995d4719eb2f4a4830637b47967cd63803f
Aswin s <aswinsudhakaran98@gmail.com>
2023-03-07 00:31:52 +0530

    created-missing-api

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,10 +1,11 @@
 const express=require('express');
-const { createCoupon, getAllCoupons, updateCoupons, deleteCoupons } = require('../controller/couponCtrl');
+const { createCoupon, getAllCoupons, updateCoupons, deleteCoupons, getCoupon } = require('../controller/couponCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 const router=express.Router()
 
 router.post('/',authMiddleware,isAdmin,createCoupon);
 router.get('/',authMiddleware,isAdmin,getAllCoupons);
+router.get('/:id',authMiddleware,isAdmin,getCoupon);
 router.put('/:id',authMiddleware,isAdmin,updateCoupons);
 router.delete('/:id',authMiddleware,isAdmin,deleteCoupons);
 module.exports=router;
