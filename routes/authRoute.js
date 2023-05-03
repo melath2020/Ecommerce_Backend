ea5d49487f921ca2d032a486d3dd9f3f293a6439
Aswin s <aswinsudhakaran98@gmail.com>
2023-05-04 00:06:14 +0530

    update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,15 +10,15 @@ router.put('/password',authMiddleware,updatePassword)
 router.post('/login',loginUserCtrl);
 router.post('/admin-login',loginAdmin);
 router.post('/cart',authMiddleware,userCart );
-router.post('/cart/applycoupon',authMiddleware,applyCoupon );
-router.post('/cart/cash-order',authMiddleware,createOrder );
-router.get('/all-users',getallUser);
-router.get('/get-orders',authMiddleware,getOrders);
-router.get('/getallorders',authMiddleware,isAdmin,getAllOrders);
-router.post('/getorderbyuser/:id',authMiddleware,isAdmin,getOrderByUserId);
-router.get("/refresh", handleRefreshToken);
-router.post("/forgot-password-token",forgotPasswordToken)
-router.put("/order/update-order/:id",authMiddleware,isAdmin,updateOrderStatus)
+router.post('/cart/create-order',authMiddleware,createOrder);
+// router.post('/cart/applycoupon',authMiddleware,applyCoupon );
+// router.get('/all-users',getallUser);
+// router.get('/get-orders',authMiddleware,getOrders);
+// router.get('/getallorders',authMiddleware,isAdmin,getAllOrders);
+// router.post('/getorderbyuser/:id',authMiddleware,isAdmin,getOrderByUserId);
+// router.get("/refresh", handleRefreshToken);
+// router.post("/forgot-password-token",forgotPasswordToken)
+// router.put("/order/update-order/:id",authMiddleware,isAdmin,updateOrderStatus)
 router.put("/reset-password/:token",resetPassword)
 
 router.get("/logout",logout)
@@ -26,7 +26,7 @@ router.get('/wishlist',authMiddleware,getWishlist);
 router.get('/cart',authMiddleware,getUserCart);
 
 router.get('/:id',authMiddleware,isAdmin,getaUser);
-router.delete('/empty-cart',authMiddleware,emptyCart);
+// router.delete('/empty-cart',authMiddleware,emptyCart);
 router.delete('/delete-product-cart/:cartItemId',authMiddleware,removeProductFromCart);
 router.delete('/update-product-cart/:cartItemId/:newQuantity',authMiddleware,updateProductQuantityFromCart);
 router.delete('/:id',deleteaUser);
